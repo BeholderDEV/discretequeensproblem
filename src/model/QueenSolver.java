363d798c002f244f2684387adea432b9bae8c1bc
Augus-top <acpluschkat@hotmail.com>
2016-09-21 02:20:26 -0300

    Base da heurística finalizada

--- src/model/QueenSolver.java
+++ src/model/QueenSolver.java
@@ -26,19 +26,66 @@ public class QueenSolver {
     private int determinarNumeroConflitos(int linha, int coluna){
         int nc = 0;
         for (int i = 1; i < this.n; i++) {
-            
+            if(coluna + i < this.n && this.posicoesQueen[coluna + i] == linha){
+                nc++;
+            }
+            if(coluna - i >= 0 && this.posicoesQueen[coluna - i] == linha){
+                nc++;
+            }
+            if(coluna + i < this.n && this.posicoesQueen[coluna + i] == linha + i){
+                nc++;
+            }
+            if(coluna + i < this.n && this.posicoesQueen[coluna + i] == linha - i){
+                nc++;
+            }
+            if(coluna - i >= 0 && this.posicoesQueen[coluna - i] == linha + i){
+                nc++;
+            }
+            if(coluna - i >= 0 && this.posicoesQueen[coluna - i] == linha - i){
+                nc++;
+            }
         }
         return nc;
+    }
+    
+    private void definirNovaPosicao(int coluna){
+        int novaPosicao = 0;
+        int custoConflitoTemporario;
+        int novoCustoConflito = -1; // this.determinarNumeroConflitos(this.posicoesQueen[coluna], coluna);
+        
+        for (int i = 0; i < this.n; i++) {
+            custoConflitoTemporario = this.determinarNumeroConflitos(i, coluna);
+            if(novoCustoConflito == -1 || custoConflitoTemporario < novoCustoConflito){
+                novoCustoConflito = custoConflitoTemporario;
+                novaPosicao = i;
+            }
+        }
+        this.posicoesQueen[coluna] = novaPosicao;
+    }
+    
+    private boolean determinarSolucaoEncontrada(){
+        for (int i = 0; i < this.n; i++) {
+            if(this.determinarNumeroConflitos(this.posicoesQueen[i], i) != 0){
+                return false;
+            }
+        }      
+        return true;
     }
     
     // Utilizando heurística de busca local - Minimum Conflict
     public void encontrarSolucao(){
         this.posicoesQueen = new int[this.n];
+        int iteracoes = 0;
         
         this.gerarEstadoAleatorio();
         
-        
-        
+        while(!determinarSolucaoEncontrada() && iteracoes <= 50){
+            for (int i = 0; i < this.n; i++) {
+                this.definirNovaPosicao(i);
+            }
+            iteracoes++;
+        }
+        System.out.println("Numero de iterações: " + iteracoes);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if(this.posicoesQueen[j] == i){
